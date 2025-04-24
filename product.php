3e56b686bdf08cf94e6602821b2254d90337dadc
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-04-24 23:26:58 +0700

    Update product.php

--- product.php
+++ product.php
@@ -1,8 +1,16 @@
 <?php
-    $url = "https://dummyjson.com/products";
-    $response = json_decode(file_get_contents($url), true);
-    $products = $response['products'];
+    $url_product = "https://localhost:7116/api/product";
+    $url_category = "https://localhost:7116/api/category";
+    $context = stream_context_create([
+        "ssl" => [
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        ]
+    ]);
+
+    $products = json_decode(file_get_contents($url_product, false, $context), true);
 ?>
+
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -13,15 +21,22 @@
         <table class="table">
             <tr>
                 <th>ID</th>
+                <th>Category</th>
                 <th>Name</th>
+                <th>Description</th>
                 <th>Price</th>
+                <th>Stock</th>
                 <th>Actions</th>
             </tr>
-            <?php foreach($products as $product): ?>
+            <?php foreach($products as $product): 
+                 $category = json_decode(file_get_contents($url_category . "/" . $product['categoryId'], false, $context), true); ?>
             <tr>
                 <td><?php echo $product['id']?></td>
-                <td><?php echo $product['title']?></td>
+                <td><?php echo $category['name']?></td>
+                <td><?php echo $product['name']?></td>
+                <td><?php echo $product['description']?></td>
                 <td><?php echo $product['price']?></td>
+                <td><?php echo $product['stock']?></td>
                 <td>
                     <a href="update_product.php?id=<?php echo $product['id']?>" class="btn btn-secondary">Update</a>
                     <a href="delete_product.php?id=<?php echo $product['id']?>" class="btn btn-danger">Delete</a>
