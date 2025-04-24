c007dca9a2cb03e655de5cc23c29823ed3507df7
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-04-24 23:27:05 +0700

    Handle SSL

--- transaction.php
+++ transaction.php
@@ -1,7 +1,14 @@
 <?php
-    $url = "https://dummyjson.com/carts";
-    $response = json_decode(file_get_contents($url), true);
-    $transactions = $response['carts'];
+    $url_sales = "https://localhost:7208/api/sales";
+
+    $context = stream_context_create([
+        "ssl" => [
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        ]
+    ]);
+
+    $sales = json_decode(file_get_contents($url_sales, false, $context), true);
 ?>
 <html>
     <head>
@@ -9,20 +16,29 @@
     </head>
     <body class="container">
         <h1 class="text-center">Transactions</h1>
-        <?php foreach($transactions as $transaction): ?>
-        <h4>Transaction ID: <?php echo $transaction['id']?></h4>
+        <?php foreach($sales as $sale): 
+            $sale_data = json_decode(file_get_contents("https://localhost:7208/api/sales/{$sale['id']}", false, $context), true);?>
+
+        <h4>Transaction ID: <?php echo $sale_data['id']?></h4>
+        <h5>Customer ID: <?php echo $sale_data['customerId']?></h5>
+        <h5>Customer Name: <?php echo $sale_data['customerName']?></h5>
+        <h5>Sale Date: <?php echo date('d-m-Y H:i:s', strtotime($sale_data['saleDate']))?></h5>
+        <h5>Total: <?php echo $sale_data['totalAmount']?></h5>
+
         <table class="table">
             <tr>
                 <th>Product ID</th>
-                <th>User ID</th>
+                <th>Product Name</th>
                 <th>Quantity</th>
-                <th>Amount</th>
+                <th>Price</th>
+                <th>Total</th>
             </tr>
-            <?php foreach($transaction['products'] as $product): ?>
+            <?php foreach($sale_data['saleProducts'] as $product): ?>
             <tr>
-                <td><?php echo $product['id']?></td>
-                <td><?php echo $transaction['userId']?></td>
+                <td><?php echo $product['productId']?></td>
+                <td><?php echo $product['productName']?></td>
                 <td><?php echo $product['quantity']?></td>
+                <td><?php echo $product['price']?></td>
                 <td><?php echo $product['price'] * $product['quantity']?></td>
             </tr>
             <?php endforeach; ?>
