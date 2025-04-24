51f0b84ef9f7e055143548b7dfaa68d79d140bb7
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-04-24 14:47:34 +0700

    Update product.php

--- product.php
+++ product.php
@@ -4,9 +4,13 @@
     $products = $response['products'];
 ?>
 <html>
-    <body>
-        <a href="new_product.php">New Product</a>
-        <table>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    </head>
+    <body class="container">
+        <h1 class="text-center">Products</h1>
+        <a href="new_product.php" class="btn btn-primary">New Product</a>
+        <table class="table">
             <tr>
                 <th>ID</th>
                 <th>Name</th>
@@ -19,11 +23,12 @@
                 <td><?php echo $product['title']?></td>
                 <td><?php echo $product['price']?></td>
                 <td>
-                    <a href="update_product.php?id=<?php echo $product['id']?>">Update</a>
-                    <a href="delete_product.php?id=<?php echo $product['id']?>">Delete</a>
+                    <a href="update_product.php?id=<?php echo $product['id']?>" class="btn btn-secondary">Update</a>
+                    <a href="delete_product.php?id=<?php echo $product['id']?>" class="btn btn-danger">Delete</a>
                 </td>
             </tr>
             <?php endforeach; ?>
         </table>
     </body>
 </html>
+
